c90076354c655f154c2da0b65ab932e2c8abfa37
Tiago Luchini <tiago@luchini.nyc>
2022-06-08 17:59:41 -0400

    Add JS experiments

---
+++ js/src/bar.js
@@ -0,0 +1,7 @@
+// const span = require("ansispan");
+// require("colors");
+
+export default function bar() {
+  console.log("Hello World!" );//.green);
+  //  console.log(process.env.PATH);
+}
---
+++ js/src/index.js
@@ -0,0 +1,34 @@
+// import bar from './bar.js';
+import { createMachine, interpret } from 'xstate';
+
+const promiseMachine = createMachine({
+  id: 'promise',
+  initial: 'pending',
+  states: {
+    pending: {
+      on: {
+        RESOLVE: { target: 'resolved' },
+        REJECT: { target: 'rejected' }
+      }
+    },
+    resolved: {
+      type: 'final'
+    },
+    rejected: {
+      type: 'final'
+    }
+  }
+});
+
+const promiseService = interpret(promiseMachine).onTransition((state) =>
+  console.log(state.value)
+);
+
+// Start the service
+promiseService.start();
+// => 'pending'
+
+promiseService.send({ type: 'RESOLVE' });
+// => 'resolved'
+
+// bar();
