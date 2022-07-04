2fb86764b9b40c488c87ddc9fb61d10972ceb554
luchiniatwork <tiago@luchini.nyc>
2022-07-04 11:29:27 -0400

    Remove comments

--- shell.nix
+++ shell.nix
@@ -7,19 +7,8 @@ pkgs.mkShell {
   packages = [ clj ];
 
   buildInputs = [
-    pkgs.hello
-
-    #   pkgs.clojure
-    #_(import ./clojure-derivation { inherit pkgs; })
 
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
 }
-
-  #  with import <nixpkgs> {}; callPackage ./clojure.nix {}
-
-
-  #    with import <nixpkgs> {};
-
-  #    (callPackage ./clojure-derivation/default.nix { })
