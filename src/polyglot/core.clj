1ecbb916ba642f2d87801161483779f781c27fb2
luchiniatwork <tiago@luchini.nyc>
2022-07-04 11:30:24 -0400

    Add tests

--- src/polyglot/core.clj
+++ src/polyglot/core.clj
@@ -1,8 +1,9 @@
 (ns polyglot.core
-  (:refer-clojure :exclude [promise resolve])
+  (:refer-clojure :exclude [promise resolve eval])
   (:import (clojure.lang IFn)
            (java.util.function Consumer)
            (org.graalvm.polyglot Context
+                                 Context$Builder
                                  Value)
            (org.graalvm.polyglot.proxy ProxyArray
                                        ProxyExecutable
@@ -53,7 +54,6 @@
     `(reify IFn
        ~@(map invoke-arity (range 22))
        (~'applyTo [this# args#] (value->clj (apply execute ~v args#))))))
-#_(macroexpand '(reify-ifn v))
 
 (defn proxy-fn
   "Returns a ProxyExecutable instance for given function, allowing it to be
@@ -93,246 +93,46 @@
                             [k (value->clj (.getMember v k))]))
     :else (throw (Exception. "Unsupported value"))))
 
-(defn create-context []
-  ^Context (-> (Context/newBuilder (into-array ["js"]))
-               (.allowExperimentalOptions true)
-               (.option "js.interop-complete-promises" "true")
-               (.allowAllAccess true)
-               .build))
+(def ^:private default-options
+  {"js.interop-complete-promises" "true"})
+
+(defn context-from-builder [^Context$Builder builder]
+  ^Context (.build builder))
+
+(defn apply-builder-defaults
+  ([^Context$Builder builder]
+   ^Context$Builder (apply-builder-defaults builder nil))
+  ([^Context$Builder builder options]
+   ^Context$Builder (let [options' (merge default-options options)]
+                      (-> builder
+                          (.allowExperimentalOptions true)
+                          (.allowAllAccess true))
+                      (doseq [[k v] options']
+                        (.option builder k v)))))
+
+(defn create-context
+  ([langs]
+   ^Context (create-context langs nil))
+  ([langs options]
+   ^Context (let [langs' (if (seq? langs) langs [langs])
+                  builder (Context/newBuilder (into-array langs'))]
+              (apply-builder-defaults builder options)
+              (context-from-builder builder))))
 
 (defn close-context [^Context ctx]
   (.close ctx))
 
-;; TODO
-#_(macro with-context [])
+(defmacro with-context [bindings & body]
+  `(with-open ~bindings ~@body))
 
-(defn get-member [^Context ctx ^String id]
-  ^Value (.getMember (.getBindings ctx "js") id))
+(defn get-member [^String lang ^Context ctx ^String id]
+  ^Value (.getMember (.getBindings ctx lang) id))
 
-(defn put-member [^Context ctx ^String id x]
-  (.putMember (.getBindings ctx "js") id x))
+(defn put-member [^String lang ^Context ctx ^String id x]
+  (.putMember (.getBindings ctx lang) id x))
 
-(defn eval-js [ctx code]
-  ^Value (.eval ^Context ctx "js" code))
+(defn eval [^String lang ctx code]
+  ^Value (.eval ^Context ctx lang code))
 
-
-
-
-(comment
-
-  (def context (create-context))
-
-  (def js->clj (comp value->clj
-                     (partial eval-js context)))
-
-  (js->clj "[{}]")
-                                        ;=> [{}]
-
-  (js->clj "false")
-                                        ;=> false
-  (js->clj "3 / 3.33")
-                                        ;=> 0.9009009009009009
-  (js->clj "123123123123123123123123123123123")
-                                        ;=> 1.2312312312312312E32
-
-  (def doubler (js->clj "(n) => {return n * 2;}"))
-  (doubler 2)
-                                        ;=> 4
-
-  (js->clj "m = {foo: 1, bar: '2', baz: {0: false}};")
-                                        ;=> {"foo" 1, "bar" "2", "baz" {"0" false}}
-
-  (def factorial
-    (eval-js context "
-      var m = [];
-      function factorial (n) {
-        if (n == 0 || n == 1) return 1;
-        if (m[n] > 0) return m[n];
-        return m[n] = factorial(n - 1) * n;
-      }
-      x = {fn: factorial, memos: m};"))
-  ((get (value->clj factorial) "fn") 12)
-                                        ;=> 479001600
-  (get (value->clj factorial) "memos")
-                                        ;=> [nil nil 2 6 24 120 720 5040 40320 362880 3628800 39916800 479001600]
-  ((get (value->clj factorial) "fn") 24)
-                                        ;=> 6.204484017332394E23
-  (get (value->clj factorial) "memos")
-                                        ;=> [nil nil 2 6 24 120 720 5040 40320 362880 3628800 39916800 479001600 ... truncated for brevity]
-
-  (eval-js context "var foo = 0xFFFF")
-  (eval-js context "console.log(foo);")
-                                        ;=> #object[org.graalvm.polyglot.Value 0x3f9d2028 "undefined"]
-                                        ;65535
-
-  (js->clj "1 + '1'")
-                                        ;=> "11"
-  (js->clj "['foo', 10, 2].sort()")
-                                        ;=> [10 2 "foo"]
-
-  (def js-aset
-    (js->clj "(arr, idx, val) => { arr[idx] = val; return arr; }"))
-  (js-aset [1 2 3] 1 nil)
-                                        ;=> [1 nil 3]
-
-  ;; FIXME it returns differently because of keywords
-  (def js-sort
-    (js->clj "(...vs) => { return vs.sort(); }"))
-  (apply js-sort [{:b nil} \a 1 "a" "A" #{\a} :foo -1 0 {:a nil} "bar"])
-                                        ;=> [-1 0 1 "A" #{\a} :foo {:a nil} {:b nil} "a" "a" "bar"]
-
-  ;; FIXME it returns differently because of keywords
-  (def variadic-fn
-    (js->clj "(x, y, ...z) => { return [x, y, z]; }"))
-  (apply variadic-fn :foo :bar (range 3))
-                                        ;=> [:foo :bar [0 1 2]]
-  (def ->json
-    (js->clj "(x) => { return JSON.stringify(x); }"))
-  (->json [1 2 3])
-                                        ;=> "[1,2,3]"
-  (->json {"foo" 1, "bar" nil})
-                                        ;=> "{\"foo\":1,\"bar\":null}"
-  (def json->
-    (js->clj "(x) => { return JSON.parse(x); }"))
-  (json-> (->json [1 2 3]))
-                                        ;=> [1 2 3]
-  (json-> (->json {"foo" 1}))
-                                        ;=> {"foo" 1}
-
-  (def json-object
-    (js->clj "(m) => { return m.foo + m.foo; }"))
-  (json-object {"foo" 1})
-                                        ;=> 2
-
-  (def clj-lambda
-    (js->clj "
-    m = {foo: [1, 2, 3],
-         bar: {
-           baz: ['a', 'z']
-         }};
-    (fn) => { return fn(m); }
-    "))
-  (clj-lambda
-   (proxy-fn #(clojure.walk/prewalk
-               (fn [v] (if (and (vector? v)
-                                (not (map-entry? v)))
-                         (vec (reverse v))
-                         v))
-               %)))
-                                        ;=> {"foo" [3 2 1], "bar" {"baz" ["z" "a"]}}
-
-  (def clj->keywords
-    (js->clj "(m) => console.log(JSON.stringify(m))"))
-  (clj->keywords {"foo" "bar"})
-  (clj->keywords {:foo 3})
-
-  
-  (def clj->callback
-    (js->clj "(f, obj) => { console.log(obj); return f(obj); }"))
-  (clj->callback (proxy-fn #(inc (get % "foo"))) {"foo" 55})
-  
-
-  ;; FIXME: do not work
-  (def js-reduce
-    (let [js-reducer (js->clj "(f, coll) => { return coll.reduce(f); }")
-          js-reducer-init (js->clj "(f, coll, init) => { return coll.reduce(f, init); }")]
-      (fn
-        ([f coll] (js-reducer f coll))
-        ([f init coll] (js-reducer-init f coll init)))))
-
-  (js-reduce + (range 10))
-                                        ;=> 45
-  (js-reduce + -5.5 (range 10))
-                                        ;=> 39.5
-  (js-reduce (fn [acc elem]
-               (assoc acc (keyword (str elem)) (doubler elem)))
-             {}
-             (range 5))
-                                        ;=> {:0 0, :1 2, :2 4, :3 6, :4 8}
-
-  (def log-coll
-    (js->clj "(coll) => { for (i in coll) console.log(coll[i]); }"))
-
-  (log-coll [1 2 3])
-  
-  (log-coll (repeatedly 3 #(do (prn 'sleeping)
-                               (Thread/sleep 100)
-                               (rand))))
-
-  (log-coll (range 10))
-
-  (close-context context)
-  
-  )
-
-
-
-
-
-
-
-(comment
-
-  (def context (create-context))
-
-  (def js->clj (comp value->clj
-                     (partial eval-js context)))
-
-  (put-member context
-              "myAsync"
-              (async-fn (fn [resolve reject]
-                          (println "happy")
-                          (resolve [1 2 3 4 {:foo "bar"}]))))
-  
-  (def async-js-fn (eval-js context
-                            "(async function () {
-              let x = await myAsync;
-              console.log(x);
-            })"))
-
-  (def t1 (value->clj async-js-fn))
-  (t1)
-
-  
-
-  
-  (put-member context
-              "javaThen"
-              (proxy-fn (fn [& args]
-                          (println "in the proxy" args))))
-  
-  (def js-promise (eval-js
-                   context
-                   "Promise.resolve(42).then(javaThen);"))
-  
-  
-  (close-context context))
-
-
-(comment
-
-  (def context (create-context))
-
-  (def js->clj (comp value->clj
-                     (partial eval-js context)))
-
-
-  (put-member context
-              "myPromise2"
-              (proxy-fn (fn [resolve reject]
-                          (println "chamou")
-                          (Thread/sleep 500)
-                          (resolve 42))))
-  
-  
-  (def js-promise (eval-js
-                   context
-                   "new Promise(myPromise2).then(x => {
-console.log (\"aqui\");
-console.log(x);});"))
-
-
-  (close-context context)
-
-
-  )
+(defn eval-parse [^String lang ctx code]
+  (value->clj (eval lang ctx code)))
---
+++ test/polyglot/core_test.clj
@@ -0,0 +1,162 @@
+(ns polyglot.core-test
+  (:require [polyglot.core :as core]
+            [clojure.test :refer :all]))
+
+(def ^:dynamic *eval-parse* (constantly true))
+
+(def ^:dynamic *context* nil)
+
+(defn context-fixture [f]
+  (core/with-context [ctx (core/create-context "js")]
+    (binding [*context* ctx
+              *eval-parse* (partial core/eval-parse "js" ctx)]
+      (f))))
+
+(use-fixtures :each context-fixture)
+
+
+(deftest basic-expressions
+  (is [{}] (*eval-parse* "[{}]"))
+
+  (is (= false (*eval-parse* "false")))
+
+  (is 0.9009009009009009 (*eval-parse* "3 / 3.33"))
+
+  (is 1.2312312312312312E32 (*eval-parse* "123123123123123123123123123123123"))
+
+  (is 65535 (*eval-parse* "0xFFFF"))
+
+  (is {"foo" 1
+       "bar" "2"
+       "baz" {"0" false}}
+      (*eval-parse* "m = {foo: 1, bar: '2', baz: {0: false}};")))
+
+
+(deftest stdout
+  (*eval-parse* "const foo = 0xFFFF")
+  (let [out (with-out-str (*eval-parse* "console.log(foo);"))]
+    (is "65535" out)))
+
+
+(deftest js-things
+  (is "11" (*eval-parse* "1 + '1'"))
+
+  (is [10 2 "foo"]
+      (*eval-parse* "['foo', 10, 2].sort()")))
+
+
+(deftest functions
+  (let [doubler (*eval-parse* "(n) => {return n * 2;}")
+        factorial-obj (*eval-parse* "
+      var m = [];
+      function factorial (n) {
+        if (n == 0 || n == 1) return 1;
+        if (m[n] > 0) return m[n];
+        return m[n] = factorial(n - 1) * n;
+      }
+      x = {fn: factorial, memos: m};")]
+
+    (is (ifn? doubler))
+    (is 4 (doubler 2))
+
+    (is (ifn? (get factorial-obj "fn")))
+    (is 479001600 ((get factorial-obj "fn") 12))
+    (is [nil nil 2 6 24 120 720 5040 40320 362880 3628800 39916800 479001600]
+        (get factorial-obj "memos"))))
+
+
+(deftest mutability
+  (testing "mutating arg should not mutate it proper"
+    (let [js-aset (*eval-parse* "(arr, idx, val) => { arr[idx] = val; return arr; }")
+          col [1 2 3]
+          out (js-aset col 1 "foo")]
+      (is (= [1 2 3] col))
+      (is (= [1 "foo" 3] out))))
+
+  (testing "mutating in context binding does mutate it"
+    (let [js-aset (*eval-parse* "
+var myArray = [4, 5, 6];
+(idx, val) => { myArray[idx] = val; return myArray; }")
+          out (js-aset 1 "foo")]
+      (is (= [4 "foo" 6] out))
+      (js-aset 2 "bar")
+      (is (= [4 "foo" "bar"] (*eval-parse* "myArray;"))))))
+
+
+(deftest keywords
+  (let [kfn (*eval-parse* "(m) => JSON.stringify(m);")]
+    (is (= "{\"foo\":\"bar\"}"
+           (kfn {"foo" "bar"})))
+    (is (= "{\"foo\":\"bar\"}"
+           (kfn {:foo "bar"})))
+    (is (= "{\"foo/bar\":\"baz\"}"
+           (kfn {:foo/bar :baz})))))
+
+(deftest json
+  (let [stringify (*eval-parse* "(x) => JSON.stringify(x);")
+        objectify (*eval-parse* "(x) => JSON.parse(x);")
+        doublefoo (*eval-parse* "(x) => { return x.foo * 2; }")]
+    (is (= "[1,2,3]"
+           (stringify [1 2 3])))
+    (is (= "[1,\"foo\",\"foo/bar\",4]"
+           (stringify [1 "foo" :foo/bar 4])))
+    (is (= "{\"foo\":1,\"bar\":null}"
+           (stringify {:foo 1 "bar" nil})))
+    (is (= [1 2 3]
+           (-> [1 2 3] stringify objectify)))
+    (is (= [1 "foo/bar" "foo/baz" 4]
+           (-> [1 :foo/bar 'foo/baz 4] stringify objectify)))
+    (is (= 6 (doublefoo {:foo 3})))))
+
+(deftest calling-clj-functions
+  (let [mutate (*eval-parse* "var x = 10; (f) => { x = f(x); return x; }")]
+    (is (= 11 (mutate (core/proxy-fn inc))))
+    (is (= 12 (mutate (core/proxy-fn inc))))
+    (is (= 15 (mutate (core/proxy-fn #(+ 3 %))))))
+
+  (let [obj-ret (*eval-parse* "
+    m = {foo: [1, 2, 3],
+         bar: {
+           baz: ['a', 'z']
+         }};
+    (fn) => { return fn(m); }
+    ")]
+    (is (= [1 2 3 4 5]
+           (obj-ret (core/proxy-fn #(-> %
+                                        (get "foo")
+                                        (conj 4 5))))))
+    (is (= ["a" "z" "aa" "zz"]
+           (obj-ret (core/proxy-fn #(-> %
+                                        (get-in ["bar" "baz"])
+                                        (conj "aa" "zz")))))))
+
+  ;; FIXME: do not work
+  #_(let [js-reducer (let [js-reducer-fn (*eval-parse* "(f, coll) => { return coll.reduce(f); }")
+                           js-reducer-init (*eval-parse* "(f, coll, init) => { return coll.reduce(f, init); }")]
+                       (fn
+                         ([f coll] (js-reducer-fn (core/proxy-fn f) coll))
+                         ([f init coll] (js-reducer-init (core/proxy-fn f) coll init))))]
+      (is (= 45 (js-reducer + (range 10))))))
+
+
+(deftest variadic-fns
+  (let [js-sort (*eval-parse* "(...vs) => { return vs.sort(); }")
+        variadic-fn (*eval-parse* "(x, y, ...z) => { return [x, y, z]; }")]
+    (is (= [-1 0 1 "A" {"b" nil} {"a" nil} "a" "a" ["a"] "bar" "foo"]
+           (apply js-sort [{:b nil} \a 1 "a" "A" #{\a} :foo -1 0 {:a nil} "bar"])))
+    (is (= ["foo/baz" "bar" [0 1 2]]
+           (apply variadic-fn :foo/baz 'bar (range 3))))))
+
+
+(deftest iterator
+  (*eval-parse* "var myArray = new Array();")
+  (let [log-coll (*eval-parse* "(coll) => { for (i in coll) myArray.push(coll[i]); }")]
+    (is (= [1 2 3]
+           (do (log-coll [1 2 3])
+               (*eval-parse* "myArray"))))
+    (is (= [1 2 3 0 1 2]
+           (do (log-coll (range 3))
+               (*eval-parse* "myArray"))))
+    (is (= [1 2 3 0 1 2 5 5 5]
+           (do (log-coll (repeatedly 3 (constantly 5)))
+               (*eval-parse* "myArray"))))))
